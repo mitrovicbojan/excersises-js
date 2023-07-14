0736d9e1efe494570d8312cad05df7675bd646a9
unknown <mitrovicbojan87@gmail.com>
2023-07-14 21:49:18 +0300

    result

--- app.js
+++ app.js
@@ -4,3 +4,24 @@
 //   card.classList.toggle("inactive");
 //   console.log("click");
 // });
+
+const fruit = {
+  apple: 5,
+  orange: 10,
+  pineapple: 30,
+  banana: 16,
+};
+
+// console.log(Object.keys(fruit));
+
+function myFunction() {
+  let selectValue = document.getElementById("mySelect").value;
+  let output = document.getElementById("tag");
+  let result = 0;
+  for (let i in fruit) {
+    if (i === selectValue) {
+      result += fruit[selectValue];
+    }
+  }
+  output.innerText = result;
+}
