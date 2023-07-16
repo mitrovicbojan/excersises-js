fda3c2662bc40ec2d2ac799de734426a1b99e133
unknown <mitrovicbojan87@gmail.com>
2023-07-16 16:20:17 +0300

    prototype

--- prototypes.Obj.js
+++ prototypes.Obj.js
@@ -3,8 +3,33 @@ function Ship(name, size) {
   this.price = 12000;
   this.size = size;
 }
-Ship.prototype.price; //no need to assign. if assinged - it will not overwritte
+Ship.prototype.color = "green";
 
 let oilTanker = new Ship("Snoopy", "80m");
+console.log(oilTanker); //prototype: Object
+console.log(oilTanker.color); //green
+// oilTanker.color = "blue";
+console.log(oilTanker); //Ship {name: 'Snoopy', price: 12000, size: '80m', color: 'blue'}
 
-console.log(oilTanker);
+// loop - own properties - prototype properties
+
+let ownProperties = [];
+let prototypeProperties = [];
+
+for (let props in oilTanker) {
+  // if (oilTanker.hasOwnProperty(props)) {
+  //   ownProperties.push(props);
+  // } else {
+  //   prototypeProperties.push(props);
+  // }
+  oilTanker.hasOwnProperty(props)
+    ? ownProperties.push(props)
+    : prototypeProperties.push(props);
+}
+console.log(ownProperties);
+console.log(prototypeProperties);
+
+//constructor property
+
+console.log(oilTanker.constructor === Ship); //true
+console.log(oilTanker instanceof Ship); //true - better to use this one
