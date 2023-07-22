40f1022033dd375e9997150e179718d1236697c0
unknown <mitrovicbojan87@gmail.com>
2023-07-22 20:06:29 +0300

    supertype

--- supertypes.js
+++ supertypes.js
@@ -1,31 +1,57 @@
-function Ship() {}
+// function Ship() {}
 
-Ship.prototype = {
-  constructor: Ship,
-  flag: function () {
-    return "Greece";
+// Ship.prototype = {
+//   constructor: Ship,
+//   flag: function () {
+//     return "Greece";
+//   },
+// };
+
+// let oilTanker = Object.create(Ship.prototype);
+// console.log(oilTanker);
+
+function Watercraft() {}
+
+Watercraft.prototype = {
+  constructor: Watercraft,
+  color: function () {
+    return "White";
   },
 };
 
-let oilTanker = Object.create(Ship.prototype);
-console.log(oilTanker);
+function Ship(name) {
+  this.name = name;
+}
 
-function House() {}
+Ship.prototype = Object.create(Watercraft.prototype);
 
-House.prototype = {
-  constructor: House,
-  street: function () {
-    return "1st street";
-  },
-};
-console.log(House.prototype.street()); //1st street
-function ParentsHouse() {}
-ParentsHouse.prototype = Object.create(House.prototype);
-console.log(ParentsHouse.prototype.street()); //1st street
-let myHouse = new ParentsHouse();
+let oilTanker = new Ship("Tesla");
 
-console.log(myHouse.street()); //1st street
+console.log(oilTanker.color()); // White
 
-//Reset an Inherited Constructor Property
-ParentsHouse.prototype.constructor = ParentsHouse;
-console.log(myHouse.constructor); //ƒ ParentsHouse() {}
+// let oilTanker = Object.create(Watercraft.prototype);
+
+// let cruiseShip = Object.create(Watercraft.prototype);
+
+// console.log(oilTanker.color()); // White
+// console.log(oilTanker instanceof Watercraft); //true
+
+// function House() {}
+
+// House.prototype = {
+//   constructor: House,
+//   street: function () {
+//     return "1st street";
+//   },
+// };
+// console.log(House.prototype.street()); //1st street
+// function ParentsHouse() {}
+// ParentsHouse.prototype = Object.create(House.prototype);
+// console.log(ParentsHouse.prototype.street()); //1st street
+// let myHouse = new ParentsHouse();
+
+// console.log(myHouse.street()); //1st street
+
+// //Reset an Inherited Constructor Property
+// ParentsHouse.prototype.constructor = ParentsHouse;
+// console.log(myHouse.constructor); //ƒ ParentsHouse() {}
