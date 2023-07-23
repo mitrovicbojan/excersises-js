7fcf2b81f23213cf94c99f35d922759624a7120f
unknown <mitrovicbojan87@gmail.com>
2023-07-23 18:40:31 +0300

    iiffe

--- palindrome.js
+++ palindrome.js
@@ -13,3 +13,20 @@ function palindrome(str) {
 }
 
 palindrome("eye%^&%^ of #the tiger");
+
+// let myStr = "John";
+
+// let newStr = "";
+// for (let i = 0; i <= myStr.length - 1; i++) {
+//   newStr = myStr[i] + newStr;
+// }
+
+// console.log(newStr);
+
+// for (let k = 0; k < myStr.length; k++) {
+//   if (myStr[k] == newStr[0 + k]) {
+//     console.log("Great");
+//   } else {
+//     console.log("not great");
+//   }
+// }
