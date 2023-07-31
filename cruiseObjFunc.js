6ce6cf53c705381a7c51708beafff2c46d67d7c7
unknown <mitrovicbojan87@gmail.com>
2023-07-31 21:33:56 +0300

    loop

--- cruiseObjFunc.js
+++ cruiseObjFunc.js
@@ -1,17 +1,13 @@
-function House() {
-  let color = "white";
-  this.getHouseColor = function () {
-    return color;
-  };
-}
-
-let apartment = new House();
-
-console.log(apartment.getHouseColor());
-
 function Destination() {
   let tahiti = {
-    A: 10,
+    A: [
+      {
+        price: 1000,
+      },
+      {
+        availableCabins: 5,
+      },
+    ],
     B: 5,
     C: 6,
   };
@@ -26,5 +22,5 @@ console.log(cruise.getCabins());
 let cabinsCat = cruise.getCabins();
 
 for (let i in cabinsCat) {
-  console.log(cabinsCat[i]);
+  console.log(cabinsCat.A[i]);
 }
