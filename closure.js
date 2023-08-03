88a1a03cd53890d5c2a7f3e1d7250e2e6f2b725d
unknown <mitrovicbojan87@gmail.com>
2023-08-04 00:18:34 +0300

    debug

--- closure.js
+++ closure.js
@@ -8,5 +8,24 @@ function House() {
 }
 
 let apartment = new House();
+// apartment.color = "black";
+apartment.price = 100;
+console.log(apartment.color);
+console.log(apartment.hasOwnProperty("color"));
+console.log(apartment.price);
+console.log(apartment.getHouseColor()); //output: white
 
-console.log(appartment.getHouseColor());
+// not private
+function Ship(name, size) {
+  this.name = name;
+  this.price = 12000;
+  this.size = size;
+}
+// Ship.prototype.color = "green";
+
+let oilTanker = new Ship("Tesla", "80m");
+
+Ship.price = 100;
+console.log(Ship.price);
+console.log(oilTanker.price);
+console.log(Ship);
