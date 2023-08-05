569ae1adfc5f025af3021b6dfd50c4d881b9d899
unknown <mitrovicbojan87@gmail.com>
2023-08-05 22:10:15 +0300

    set range

--- interAlgo/diffTwoArrays.js
+++ interAlgo/diffTwoArrays.js
@@ -5,16 +5,21 @@
 // Note: You can return the array with its elements in any order.
 function diffArray(arr1, arr2) {
   const newArr = [];
-  for (let i in arr2) {
-    if (!arr1.includes(arr2[i])) {
-      newArr.push(arr2[i]);
+  for (let i in arr1) {
+    if (!arr2.includes(arr1[i])) {
+      newArr.push(arr1[i]);
     }
   }
   return newArr;
 }
 
 diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
-console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+console.log(
+  diffArray(
+    ["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"],
+    ["diorite", "andesite", "grass", "dirt", "dead shrub"]
+  )
+);
 
 let firstArr = [1, 2, 3, 5];
 let secondArr = [1, 2, 3, 4, 5];
