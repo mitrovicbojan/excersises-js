7127ae6da073fd0eec21f61853a1d27e1097b356
unknown <mitrovicbojan87@gmail.com>
2023-08-05 21:15:37 +0300

    includes

--- interAlgo/diffTwoArrays.js
+++ interAlgo/diffTwoArrays.js
@@ -5,20 +5,22 @@
 // Note: You can return the array with its elements in any order.
 function diffArray(arr1, arr2) {
   const newArr = [];
+  for (let i in arr2) {
+    if (!arr1.includes(arr2[i])) {
+      newArr.push(arr2[i]);
+    }
+  }
   return newArr;
 }
 
 diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
 
 let firstArr = [1, 2, 3, 5];
 let secondArr = [1, 2, 3, 4, 5];
-let myArr = [];
 let newArr = [];
-let countLoop = 0;
 for (let i in secondArr) {
-  if (firstArr.includes(secondArr[i])) {
-    console.log(secondArr[i]);
-  } else {
+  if (!firstArr.includes(secondArr[i])) {
     newArr.push(secondArr[i]);
   }
 }
