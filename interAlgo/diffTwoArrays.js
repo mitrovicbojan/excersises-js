3c5adaf18fdf12d544d77fc72b525d9172930e42
unknown <mitrovicbojan87@gmail.com>
2023-08-06 11:37:53 +0300

    diffArr

--- interAlgo/diffTwoArrays.js
+++ interAlgo/diffTwoArrays.js
@@ -4,6 +4,17 @@
 // In other words, return the symmetric difference of the two arrays.
 // Note: You can return the array with its elements in any order.
 function diffArray(arr1, arr2) {
+  let longerArr = [];
+  let shorterArr = [];
+  if (arr1.length >= arr2.length) {
+    longerArr.push(arr1);
+    shorterArr.push(arr2);
+  } else {
+    longerArr.push(arr2);
+    shorterArr.push(arr1);
+  }
+  console.log(longerArr);
+  console.log(shorterArr);
   const newArr = [];
   for (let i in arr1) {
     if (!arr2.includes(arr1[i])) {
@@ -13,7 +24,7 @@ function diffArray(arr1, arr2) {
   return newArr;
 }
 
-diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+// diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
 console.log(
   diffArray(
     ["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"],
@@ -21,12 +32,31 @@ console.log(
   )
 );
 
-let firstArr = [1, 2, 3, 5];
-let secondArr = [1, 2, 3, 4, 5];
-let newArr = [];
-for (let i in secondArr) {
-  if (!firstArr.includes(secondArr[i])) {
-    newArr.push(secondArr[i]);
+//resenje
+function diffArray(arr1, arr2) {
+  const newArr = [];
+
+  for (let i in arr1) {
+    if (!arr2.includes(arr1[i])) {
+      newArr.push(arr1[i]);
+    }
   }
+  for (let i in arr2) {
+    if (!arr1.includes(arr2[i])) {
+      newArr.push(arr2[i]);
+    }
+  }
+
+  return newArr;
 }
-console.log(newArr);
+console.log(diffArray([1, "calf", 3, "piglet"], [7, "filly"]));
+
+// let firstArr = [1, 2, 3, 5];
+// let secondArr = [1, 2, 3, 4, 5];
+// let newArr = [];
+// for (let i in secondArr) {
+//   if (!firstArr.includes(secondArr[i])) {
+//     newArr.push(secondArr[i]);
+//   }
+// }
+// console.log(newArr);
