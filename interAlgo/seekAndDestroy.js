80aa59bcf0497a36468d8d9a5c769568c1a718d2
unknown <mitrovicbojan87@gmail.com>
2023-09-17 21:15:39 +0300

    stuck'

--- interAlgo/seekAndDestroy.js
+++ interAlgo/seekAndDestroy.js
@@ -1,7 +1,18 @@
 // Seek and Destroy
 
-function destroyer(arr) {
-  console.log(arr);
+function destroyer(...args) {
+  console.log(args);
+  let newArr = args.splice(0, 1);
+  let restArr = args.splice(0, args.length);
+  let firstArr = newArr[0];
+  let outPut = [];
+  console.log(newArr);
+  console.log(firstArr);
+
+  for (let i in firstArr) {
+    if ((firstArr[i] = restArr[i])) {
+    }
+  }
 }
 
 destroyer([1, 2, 3, 1, 2, 3], 2, 3);
