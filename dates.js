e200c7dfb80b8c91cc4889d5862a99415a96cf12
unknown <mitrovicbojan87@gmail.com>
2023-10-03 22:39:11 +0300

    date

--- dates.js
+++ dates.js
@@ -1,4 +1,4 @@
-let newDay = 15;
+let newDay = 30;
 let newMonth = "5";
 let myYear = 1996;
 
@@ -9,3 +9,6 @@ console.log(myDate);
 let newYear = 2001;
 let newDate = myDate.getFullYear();
 console.log(newYear - newDate);
+
+let checkDate = new Date("2023/02/31");
+console.log(checkDate.getFullYear());
