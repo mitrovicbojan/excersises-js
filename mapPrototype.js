f7ce2d0f8cbc326582f35b1216fe8aed18c38231
unknown <mitrovicbojan87@gmail.com>
2023-10-15 21:11:43 +0300

    prototype

--- mapPrototype.js
+++ mapPrototype.js
@@ -1,1 +1,10 @@
 //Implement map on a Prototype
+Array.prototype.myMap = function (callback) {
+  const newArray = [];
+  // Only change code below this line
+  for (let i = 0; i < this.length; i++) {
+    newArray.push(callback(this[i], i, this));
+  }
+  // Only change code above this line
+  return newArray;
+};
