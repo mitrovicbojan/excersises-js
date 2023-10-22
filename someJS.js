2c2b1aa5609715a398d8d2335054a8bfc0952db1
unknown <mitrovicbojan87@gmail.com>
2023-10-22 15:46:21 +0300

    some

--- someJS.js
+++ someJS.js
@@ -5,6 +5,21 @@
 
 const numbers = [10, 50, 8, 220, 110, 11];
 
-numbers.some(function (currentValue) {
+let num = numbers.some(function (currentValue) {
   return currentValue < 10;
 });
+
+console.log(num);
+
+function checkPositive(arr) {
+  // Only change code below this line
+  let myArr = arr;
+
+  let positiveNum = myArr.some(function (val) {
+    return val > 0;
+  });
+  return positiveNum;
+  // Only change code above this line
+}
+
+checkPositive([1, 2, 3, -4, 5]);
