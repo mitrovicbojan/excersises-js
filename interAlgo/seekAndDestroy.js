b526b2206a0fdc721eb59b16c0b46e40ea56a893
unknown <mitrovicbojan87@gmail.com>
2023-10-22 19:08:46 +0300

    spinalTapCase

--- interAlgo/seekAndDestroy.js
+++ interAlgo/seekAndDestroy.js
@@ -12,3 +12,14 @@ function destroyer(...args) {
 
 destroyer([1, 2, 3, 1, 2, 3], 2, 3);
 console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
+
+function destroyer(...args) {
+  let newArr = [...args[0]];
+  let restArr = args.slice(1, args.length);
+
+  let filtered = newArr.filter((value) => !restArr.includes(value));
+  return filtered;
+}
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
--- interAlgo/spinalTapCase.js
+++ interAlgo/spinalTapCase.js
@@ -1,52 +1,15 @@
 //Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
 
-// function spinalCase(str) {
-//   let mystr = "";
-//   for (let i in str) {
-//     mystr += str[i].replace("_", " ");
-//   }
-//   console.log(mystr);
-//   let arr = mystr.split(" ");
-//   console.log(arr);
-//   let newStr = "";
-
-//   newStr = arr.join("-");
-//   let extraStr = "";
-//   for (let i = 0; i < newStr.length; i++) {
-//     if (newStr[i] == newStr[i].toUpperCase()) {
-//       extraStr += newStr[i].toUpperCase();
-//       console.log("up");
-//     }
-//   }
-//   console.log(extraStr);
-//   return newStr;
-// }
-
-// console.log(spinalCase("AllThe-small Things"));
-
-let strHolder = "AllThe-small Things";
-
-let newStrHolder = strHolder[0];
-
-for (let i = 1; i < strHolder.length; i++) {
-  if (strHolder[i] == strHolder[i].toLowerCase()) {
-    newStrHolder += strHolder[i];
-  } else if (strHolder[i] == strHolder[i].toUpperCase()) {
-    newStrHolder += " " + strHolder[i];
+function spinalCase(str) {
+  // str.replace(/([a-z\d])([A-Z])/g, "$1 $2");
+  // console.log(str.replace(/([a-z\d])([A-Z])/g, "$1 $2"));
+  let arr = str.replace(/([a-z\d])([A-Z])/g, "$1 $2").split(/\W|_/g);
+  let newStr = "";
+  for (let i in arr) {
+    newStr = arr.join("-").toLowerCase();
   }
-}
-let removingStr = "";
-for (let i in newStrHolder) {
-  removingStr += newStrHolder[i].replace("_", "");
-}
-for (let i in newStrHolder) {
-  removingStr += newStrHolder[i].replace(" ", "");
+  return newStr;
 }
 
-console.log(newStrHolder);
-console.log(removingStr);
-
-let splitArr = newStrHolder.split(" ");
-console.log(splitArr);
-let joinedStr = splitArr.join("");
-console.log(joinedStr);
+spinalCase("AllThe-small Things");
+// console.log(spinalCase("AllThe-small Things"));
