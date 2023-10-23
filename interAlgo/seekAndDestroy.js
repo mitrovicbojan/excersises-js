44fcbfadea80399a2371785d1c98d2109c13ce5e
unknown <mitrovicbojan87@gmail.com>
2023-10-23 23:25:39 +0300

    where

--- interAlgo/seekAndDestroy.js
+++ interAlgo/seekAndDestroy.js
@@ -13,9 +13,13 @@ function destroyer(...args) {
 destroyer([1, 2, 3, 1, 2, 3], 2, 3);
 console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
 
-function destroyer(...args) {
-  let newArr = [...args[0]];
-  let restArr = args.slice(1, args.length);
+function destroyer(arr, ...args) {
+  let newArr = arr;
+  //  [...args[0]];
+  console.log(newArr);
+  let restArr = args;
+  console.log(restArr);
+  // let restArr = args.slice(1, args.length);
 
   let filtered = newArr.filter((value) => !restArr.includes(value));
   return filtered;
---
+++ interAlgo/whereforeArtThou.js
@@ -0,0 +1,19 @@
+//Wherefore art thou
+
+function whatIsInAName(collection, source) {
+  let newArr = [...collection];
+
+  for (let i = 0; i < newArr.length; i++) {
+    for (let k in source) {
+      if (!newArr[i].hasOwnProperty(k) || newArr[i][k] != source[k]) {
+        newArr.splice(i, 1);
+        i--;
+        break;
+      }
+    }
+  }
+  console.log(newArr);
+  return newArr;
+}
+
+whatIsInAName([{ a: 1, b: 2, c: 3 }], { a: 1, b: 9999, c: 3 });
