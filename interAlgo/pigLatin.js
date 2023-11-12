bc77e445d0646c3a0d9eb721aab1ce9064f52e8d
unknown <mitrovicbojan87@gmail.com>
2023-11-12 22:32:38 +0200

    pig

--- interAlgo/pigLatin.js
+++ interAlgo/pigLatin.js
@@ -3,15 +3,21 @@
 let vowelArr = ["a", "e", "i", "o", "u"];
 
 function translatePigLatin(str) {
-  let newStr = "";
-  let cons = "";
-  let rest = "";
-  for (let i = 0; i < vowelArr.length; i++) {
-    if (vowelArr[i] == str[0]) {
-      newStr += str;
-      return newStr + "way";
+  let inx = 1;
+  let newStr = str.slice(0, inx);
+  let rest = str.slice(inx);
+
+  if (vowelArr.includes(str[0])) {
+    return str + "way";
+  }
+  for (let i = 0; i < str.length; i++) {
+    if (!vowelArr.includes(str[i])) {
+      console.log(i);
+      inx + 1;
     }
   }
+  console.log(inx);
+  return rest + newStr + "ay";
 }
 
 console.log(translatePigLatin("california"));
