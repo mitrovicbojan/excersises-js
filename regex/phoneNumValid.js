042e22e49ded3c9fbcd441f8893f11448a3ccb0e
unknown <mitrovicbojan87@gmail.com>
2023-11-19 17:23:00 +0200

    phone

---
+++ regex/phoneNumValid.js
@@ -0,0 +1,15 @@
+//Telephone Number Validator
+
+// 555-555-5555
+// (555)555-5555
+// (555) 555-5555
+// 555 555 5555
+// 5555555555
+// 1 555 555 5555
+// return true or false
+
+function telephoneCheck(str) {
+  return true;
+}
+
+telephoneCheck("555-555-5555");
