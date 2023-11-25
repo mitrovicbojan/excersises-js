526b075fc3f987fb34c4c589110575f3cbd547ce
unknown <mitrovicbojan87@gmail.com>
2023-11-25 21:33:26 +0200

    if

--- regex/phoneNumValid.js
+++ regex/phoneNumValid.js
@@ -9,7 +9,21 @@
 // return true or false
 
 function telephoneCheck(str) {
-  return true;
+  let myReg = new RegExp(
+    "^[1-9]{1}[0-9]{0,2}[2-9]{1}[0-9]{2}[2-9]{1}[0-9]{2} [0-9]{4}$|^[1-9]{1}[0-9]{0,2}[2-9]{1}[0-9]{2}[2-9]{1}[0-9]{2}[0-9]{4}$"
+  );
+  if (myReg.test(str)) {
+    console.log("Phone number is valid");
+    return true;
+  } else {
+    console.log("Phone number is not valid");
+    return false;
+  }
 }
 
-telephoneCheck("555-555-5555");
+// telephoneCheck("555-555-5555");
+console.log(telephoneCheck("15555555555"));
+
+// let myReg = new RegExp(
+//     "^[1-9]{1}[0-9]{0,2}-[2-9]{1}[0-9]{2}-[2-9]{1}[0-9]{2}-[0-9]{4}$"
+//   );
