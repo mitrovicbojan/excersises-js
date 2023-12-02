070dd45c274ff4c645a1147b014b44406f1c7c31
unknown <mitrovicbojan87@gmail.com>
2023-12-02 21:59:23 +0200

    stuck

--- interAlgo/argumentsOptional.js
+++ interAlgo/argumentsOptional.js
@@ -2,7 +2,18 @@
 
 function addTogether() {
   let props = [...arguments]; //ju ladno!! slucajno sam nasao ovu foru
+  let firstNum = props[0];
+  let secondNum = props[1];
   console.log(props);
+  console.log(firstNum);
+  console.log(secondNum);
+
+  if ((typeof firstNum === "number") & (typeof secondNum === "number")) {
+    return firstNum + secondNum;
+  }
+  if (props.length === 1) {
+    return "shit";
+  }
 }
 
-console.log(addTogether(2, 3));
+console.log(addTogether(2));
