23968e995dbf6d1b28dd831d8405b0afc2689b37
unknown <mitrovicbojan87@gmail.com>
2023-12-04 22:24:45 +0200

    scn

--- interAlgo/makeaPerson.js
+++ interAlgo/makeaPerson.js
@@ -1,8 +1,40 @@
 //Make a Person
 
 const Person = function (first, last) {
+  let firstName = first;
+  let lastName = last;
+  console.log(firstName);
+
+  this.getFirstName = function () {
+    return firstName;
+  };
+  this.getLastName = function () {
+    return lastName;
+  };
+
   this.getFullName = function () {
-    return "";
+    return console.log(this.getFirstName() + " " + this.getLastName()); //prvo sam stavio this.first i this.last
+  };
+
+  //return console.log(this.getFirstName() + " " + this.getLastName());
+  // ostavio sam console log kad sam stavio resenje na sajtu pa je preskako...
+  this.setFirstName = function (first) {
+    return (firstName = first);
+  };
+
+  this.setLastName = function (last) {
+    return (lastName = last);
+  };
+
+  this.setFullName = function (first, last) {
+    this.setFirstName(first);
+    this.setLastName(last);
+    return this.getFullName();
   };
   return "";
 };
+
+let myName = new Person("Pera", "Zdera");
+console.log(myName.setFirstName("djole"));
+console.log(myName.setFullName("dzoni", "b"));
+console.log(myName.getFullName());
