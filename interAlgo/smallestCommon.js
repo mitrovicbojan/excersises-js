98b93412622063ee94b5bcadf708ce2e6e7d1bb9
unknown <mitrovicbojan87@gmail.com>
2023-12-08 21:44:10 +0200

    stuck

--- interAlgo/smallestCommon.js
+++ interAlgo/smallestCommon.js
@@ -1,10 +1,24 @@
 function smallestCommons(arr) {
-  let range = [];
-  for (let i = arr[0]; i <= arr[1]; i++) {
-    range.push(i);
-  }
-  console.log(range);
+  // let range = [];
+  // for (let i = arr[0]; i <= arr[1]; i++) {
+  //   range.push(i);
+  // }
+  // console.log(range);
 
+  // let output = range.forEach((item) => {
+  //   if (item % 2 === 0) {
+  //     console.log(item);
+  //   }
+  // });
+
+  let smallerNum = Math.min(arr[0], arr[1]);
+  let largerNum = Math.max(arr[0], arr[1]);
+  console.log(smallerNum);
+  console.log(largerNum);
+  // range??
+  // for (let i = largerNum; i <= largerNum; i++) {
+
+  // }
   return arr;
 }
 
