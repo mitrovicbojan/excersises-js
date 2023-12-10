17d9a4e6cd37782a73d45f7247a625fab67b68c7
unknown <mitrovicbojan87@gmail.com>
2023-12-10 16:58:18 +0200

    deObj

---
+++ dataStructures/destructuringObj.js
@@ -0,0 +1,86 @@
+"use strict";
+
+// Data needed for a later exercise
+// const flights =
+//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// Data needed for first part of the section
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = "20:00",
+    address,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be deliverd to ${address} at ${time}`
+    );
+  },
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
+
+restaurant.orderDelivery({
+  time: "22:30",
+  address: "1st street",
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+  address: "2nd street",
+});
+//destructuring Obj
+const { name, openingHours, categories } = restaurant;
+
+console.log(name, openingHours, categories);
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+//default values
+
+const { menu = [], starterMenu: starters = [] } = restaurant;
+
+console.log(starters);
+
+//mutating variables
+
+let a = 111;
+let b = 999;
+const myObj = { a: 23, b: 7, c: 14 };
+({ a, b } = myObj);
+console.log(a, b);
+
+//nested obj
+
+const {
+  fri: { open, close },
+} = openingHours;
+console.log(open, close);
