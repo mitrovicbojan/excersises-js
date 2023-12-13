51459ac44a4bb379a23f4e2af1b4d4d33b70d6bd
unknown <mitrovicbojan87@gmail.com>
2023-12-13 22:00:48 +0200

    challenge

---
+++ dataStructures/challenge2.js
@@ -0,0 +1,53 @@
+const game = {
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
+  ],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+//1. Loop over the game.scored arry and print each player name
+// along with the goal number
+//example: "Goal 1: Lewandowski"
+
+// const playersScoredArr = game.scored;
+// console.log(playersScoredArr);
+
+for (let [goal, name] of game.scored.entries()) {
+  console.log(`Goal ${goal + 1}: ${name}`);
+}
+
+//2. Use loop o calculate the avrage odd and log it
