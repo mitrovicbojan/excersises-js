4a573111d43bca6c0fda61d05d65a11b3bb55fbf
unknown <mitrovicbojan87@gmail.com>
2023-12-20 10:00:04 +0200

    str

--- dataStructures/stringsPt2.js
+++ dataStructures/stringsPt2.js
@@ -27,3 +27,43 @@ console.log(normalizedEmail);
 console.log(email === normalizedEmail);
 
 //replacing
+
+const priceGB = "288,97£";
+const priceUS = priceGB.replace("£", "$").replace(",", ".");
+
+console.log(priceUS);
+
+const announcement =
+  "All passengers come to boarding door 12. Boarding door 23.";
+// console.log(announcement.replace("door", "gate"));
+// console.log(announcement.replaceAll("door", "gate"));
+
+//regex
+console.log(announcement.replace(/door/g, "gate"));
+
+// Booleans
+
+const newPlane = "Airbus A320neo";
+console.log(newPlane.includes("A320"));
+console.log(newPlane.includes("XX"));
+console.log(newPlane.startsWith("Airb"));
+
+if (newPlane.startsWith("Airbus") && newPlane.endsWith("neo")) {
+  console.log("Part of the new airbus family");
+}
+
+//practice
+
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase();
+  if (baggage.includes("knife") || baggage.includes("gun")) {
+    console.log("You are not allowed to board");
+  } else {
+    console.log("Welcome aboard");
+  }
+};
+checkBaggage("I have a laptop, some Food and a pocket Knife");
+
+checkBaggage("Socks and camera");
+
+checkBaggage("Got some snacks and a gun for protection");
