c9987efb14c7840395ac4cd0b65bf232e76c112d
unknown <mitrovicbojan87@gmail.com>
2023-12-23 10:38:42 +0200

    notes

--- functions/review.js
+++ functions/review.js
@@ -15,3 +15,24 @@ const carRental = {
 
 carRental.rent(10, "John Smith");
 console.log(carRental);
+
+// default parameters
+
+const reservations = [];
+
+const ferryBooking = function (paxNum, ferryName, price) {
+  const reservation = {
+    paxNum,
+    ferryName,
+    price,
+  };
+  console.log(reservation);
+  //paxNum: undefined, vehicle: undefined, price: undefined
+  reservations.push(reservation);
+};
+console.log(reservations);
+ferryBooking();
+
+//const ferryBooking = function (paxNum = 1, ferryName = "Poseidon", price = 50)
+
+//passing arguments
