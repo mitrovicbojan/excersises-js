9d8c1fec6be2a954b5d52f3355bd586d1ccc943e
unknown <mitrovicbojan87@gmail.com>
2024-01-09 21:17:38 +0200

    num

--- bankist/bankapp.js
+++ bankist/bankapp.js
@@ -88,7 +88,7 @@ const dispalyMovements = function (movements, sort = false) {
           <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-          <div class="movements__value">${mov}€</div>
+          <div class="movements__value">${mov.toFixed(2)}€</div>
         </div>
     `;
     containerMovements.insertAdjacentHTML("afterbegin", html);
@@ -125,7 +125,7 @@ const updateUI = function (acc) {
 const calcPrintBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
 
-  labelBalance.textContent = `${acc.balance} €`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)} €`;
 };
 
 // calcPrintBalance(account1.movements);
@@ -137,13 +137,13 @@ const clacDisplaySummary = function (acc) {
     .filter((mov) => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
 
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter((mov) => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
 
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
 
   const interest = acc.movements
     .filter((mov) => mov > 0)
@@ -153,7 +153,7 @@ const clacDisplaySummary = function (acc) {
     })
     .reduce((acc, interest) => acc + interest, 0);
 
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 // clacDisplaySummary(movements);
 
@@ -206,7 +206,7 @@ btnTransfer.addEventListener("click", function (e) {
 
 btnLoan.addEventListener("click", function (e) {
   e.preventDefault();
-  const amount = Number(inputLoanAmount.value);
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (
     amount > 0 &&
@@ -256,6 +256,12 @@ console.log(allMovements);
 
 const overallBalance = allMovements.reduce((acc, mov) => acc + mov, 0);
 console.log(overallBalance);
+
+labelBalance.addEventListener("click", function () {
+  [...document.querySelectorAll(".movements__row")].forEach(function (row, i) {
+    if (i % 2 === 0) row.style.backgroundColor = "orangered";
+  });
+});
 
 //chaining
 const overalBalance = accounts
--- numsDates/checkNum.js
+++ numsDates/checkNum.js
@@ -17,3 +17,36 @@ console.log(Number.isNaN(+"23x")); //true
 console.log(Number.isFinite(20)); //true
 console.log(Number.isFinite("20")); //false
 console.log(Number.isFinite(+"23x")); //false
+
+//square root
+console.log(Math.sqrt(25));
+console.log(25 ** 1 / 2); //same
+
+//max value
+console.log(Math.max(5, 18, 11, 3, 23)); //max num; no parsing
+
+//random
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min) + 1) + min;
+
+console.log(randomInt(10, 20));
+
+//rounding integers
+
+console.log(Math.trunc(23.3));
+console.log(Math.round(23.9)); //nearest int
+
+console.log(Math.ceil(23.6)); //round up
+console.log(Math.floor(23.8)); //round down
+
+//rounding decimals
+
+console.log((2.7).toFixed(0)); //returns str
+
+//remainder
+
+const isEven = (n) => n % 2 === 0;
+console.log(isEven(9));
+console.log(isEven(8));
